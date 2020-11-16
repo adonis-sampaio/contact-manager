ebeaa9e36431fdd5e5fea9c63f2dea3fc1202d75
Computador <adonissamp@gmail.com>
2020-11-16 16:47:01 -0300

    1.0

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Contact from './components/Contact';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <Header branding="Manager"/>
+        <Contact />
+       
+    </div>
+  );
+}
+
+export default App;
